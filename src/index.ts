d6ce9728c73b0a8b18dab594737de00a740d71f4
yasg-uru <yashpawar12122004@gmail.com>
2025-04-19 21:27:17 +0530

    created server setup and implemented authentication

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import ConnectDatabase from "./lib/connectDb";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import userRouter from "./route/user.route";
+
+import { ErrorhandlerMiddleware } from "./util/Errorhandler.util";
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      
+      "http://localhost:5173",
+      
+    ], // The IP address where your Expo app is running
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+
+app.use(ErrorhandlerMiddleware);
+
+dotenv.config();
+ConnectDatabase();
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("server is running on port:", PORT);
+});
---
+++ src/route/user.route.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import {
+  completeLesson,
+  forgotPassword,
+  getAllEnrolledCourseProgress,
+  LoadProgress,
+  Login,
+  Logout,
+  registerUser,
+  Resetpassword,
+  verifyuser,
+} from "../controller/user.controller";
+import upload from "../middleware/multer.middleware";
+import { isAuthenticated } from "../middleware/auth.middleware";
+
+
+const userRouter = Router();
+userRouter.post("/register", upload.single("profileUrl"), registerUser); //marked
+userRouter.post("/verify-code", verifyuser); //marked
+userRouter.post("/sign-in", Login); //marked
+userRouter.post("/logout", Logout); //marked
+userRouter.post("/forgot-password", forgotPassword); //marked
+userRouter.put("/reset-password/:token", Resetpassword); //marked
+userRouter.post(
+  "/lesson/complete/:courseId/:moduleId/:lessonId",
+  isAuthenticated,
+  completeLesson
+);
+userRouter.get(
+  "/enrolledcourse/progress/:courseId",
+  isAuthenticated,
+  LoadProgress
+);
+userRouter.get(
+  "/enrolled/progress",
+  isAuthenticated,
+  getAllEnrolledCourseProgress
+);
+
+export default userRouter;
